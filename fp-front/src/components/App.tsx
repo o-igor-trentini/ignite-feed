6279421bf6e342a797636de7896b842ec3fd1a98
Igor Trentini <igortrentini.2004@gmail.com>
2022-06-30 20:03:49 -0300

    wip: Main css

---
+++ fp-front/src/components/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from './Header';
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <h1>hello world</h1>
+        </>
+    );
+};
+
+export default App;
---
+++ fp-front/src/components/Header/index.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import styles from './index.module.css';
+
+export const Header: FC = () => {
+    return <div className={styles.header}>Header</div>;
+};
